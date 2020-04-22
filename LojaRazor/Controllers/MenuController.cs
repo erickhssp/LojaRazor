fa889c5142a529aac1671fd0be759e28ce91aa77
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-22 14:13:04 -0300

    Configurando layout global, viewstart e criando MenuController para listagem de departamentos sem repetição de código.

---
+++ LojaRazor/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using LojaRazor.DAO;
+using System.Web.Mvc;
+
+namespace LojaRazor.Controllers
+{
+    public class MenuController : Controller
+    {
+        //
+        // GET: /Menu/
+
+        public ActionResult Index()
+        {
+            DepartamentosDAO departamentosDAO = new DepartamentosDAO();
+            ViewBag.Departamentos = departamentosDAO.Lista();
+            return View();
+        }
+
+    }
+}
